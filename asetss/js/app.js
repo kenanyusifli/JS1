172715469fd38386c110ef02bfa4d36e9954e374
kenanyusifli <kenan030ss@gmail.com>
2023-11-27 10:09:34 +0400

    Js1

---
+++ asetss/js/app.js
@@ -0,0 +1,10 @@
+var name = prompt  ("How old are you?")
+if(name.lenght < 5)  {
+    console.log("kicik yas")
+}
+else if ( name.lenght < 18) {
+    console.log("orta yas")
+}
+else if (name.lenght < 65) {
+    console.log("boyuk yas")
+}
